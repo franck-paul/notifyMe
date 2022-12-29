bf7b9d27cf558fcf2d1549accc2c2e98050c5b9d
franck <carnet.franck.paul@gmail.com>
2022-12-29 13:04:42 +0100

    use dotclear.jsonServicesGet() rather than dotclear.services()

--- js/common.js
+++ js/common.js
@@ -1,38 +1,25 @@
 /*global dotclear, notifyBrowser */
 'use strict';
 
-function checkNewComments() {
-  dotclear.services(
+dotclear.notify_me.checkNewComments = () => {
+  dotclear.jsonServicesGet(
     'notifyMeCheckNewComments',
-    (data) => {
-      try {
-        const response = JSON.parse(data);
-        if (response?.success) {
-          if (Number(response?.payload.ret) > 0) {
-            notifyBrowser(response.payload.msg, dotclear.notify_me.config.title, dotclear.notify_me.config.wait);
-            dotclear.notify_me.comments.id = response?.payload.last_id;
-          }
-        } else {
-          console.log(dotclear.debug && response?.message ? response.message : 'Dotclear REST server error');
-          return;
-        }
-      } catch (e) {
-        console.log(e);
+    (payload) => {
+      if (payload.ret) {
+        notifyBrowser(response.payload.msg, dotclear.notify_me.config.title, dotclear.notify_me.config.wait);
+        dotclear.notify_me.comments.id = payload.last_id;
       }
     },
-    (error) => {
-      console.log(error);
-    },
-    true, // Use GET method
     {
-      json: 1, // Use JSON format for payload
       last_id: dotclear.notify_me.comments.id,
     },
   );
-}
+};
 
 window.addEventListener('load', () => {
   dotclear.notify_me.comments = dotclear.getData('notify_me_comments');
+  // First check
+  dotclear.notify_me.checkNewComments();
   // Set interval between two checks for new comments
-  setInterval(checkNewComments, dotclear.notify_me.comments.check);
+  setInterval(dotclear.notify_me.checkNewComments, dotclear.notify_me.comments.check);
 });
