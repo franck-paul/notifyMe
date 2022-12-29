699b254334c1826202b63d512cb3ebb84a9dd97a
franck <carnet.franck.paul@gmail.com>
2022-12-29 08:53:13 +0100

    Release 1.1

--- _define.php
+++ _define.php
@@ -18,20 +18,20 @@ $this->registerModule(
     'Browser notifications',
     'Display notifications in your web browser',
     'Franck Paul and contributors',
-    '1.0',
+    '1.1',
     [
         'requires'    => [['core', '2.24']],
         'permissions' => dcCore::app()->auth->makePermissions([
             dcAuth::PERMISSION_USAGE,
             dcAuth::PERMISSION_CONTENT_ADMIN,
         ]),
-        'type'     => 'plugin',
-        'settings' => [
+        'type'        => 'plugin',
+        'settings'    => [
             'pref' => '#user-options.notify-me',
         ],
 
-        'details'    => 'https://open-time.net/?q=notifyMe',
-        'support'    => 'https://github.com/franck-paul/notifyMe',
-        'repository' => 'https://raw.githubusercontent.com/franck-paul/notifyMe/master/dcstore.xml',
+        'details'     => 'https://open-time.net/?q=notifyMe',
+        'support'     => 'https://github.com/franck-paul/notifyMe',
+        'repository'  => 'https://raw.githubusercontent.com/franck-paul/notifyMe/master/dcstore.xml',
     ]
 );
