e457b158aac20a5df00378e48b9097567749b47d
franck <carnet.franck.paul@gmail.com>
2022-12-29 13:04:42 +0100

    use dotclear.jsonServicesGet() rather than dotclear.services()

--- js/common.js
+++ js/common.js
@@ -1,38 +1,28 @@
 /*global dotclear, notifyBrowser */
 'use strict';
 
-function checkNewComments() {
-  dotclear.services(
-    'notifyMeCheckNewComments',
-    (data) => {
-      try {
-        const response = JSON.parse(data);
-        if (response?.success) {
-          if (Number(response?.payload.ret) > 0) {
-            notifyBrowser(response.payload.msg, dotclear.notify_me.config.title, dotclear.notify_me.config.wait);
-            dotclear.notify_me.comments.id = response?.payload.last_id;
-          }
-        } else {
-          console.log(dotclear.debug && response?.message ? response.message : 'Dotclear REST server error');
-          return;
-        }
-      } catch (e) {
-        console.log(e);
-      }
-    },
-    (error) => {
-      console.log(error);
-    },
-    true, // Use GET method
-    {
-      json: 1, // Use JSON format for payload
-      last_id: dotclear.notify_me.comments.id,
-    },
-  );
-}
-
 window.addEventListener('load', () => {
+  // Load settings
   dotclear.notify_me.comments = dotclear.getData('notify_me_comments');
+
+  // Helper
+  dotclear.notify_me.checkNewComments = () => {
+    dotclear.jsonServicesGet(
+      'notifyMeCheckNewComments',
+      (payload) => {
+        if (payload.ret) {
+          notifyBrowser(payload.msg, dotclear.notify_me.config.title, dotclear.notify_me.config.wait);
+          dotclear.notify_me.comments.id = payload.last_id;
+        }
+      },
+      {
+        last_id: dotclear.notify_me.comments.id,
+      },
+    );
+  };
+
+  // First check
+  dotclear.notify_me.checkNewComments();
   // Set interval between two checks for new comments
-  setInterval(checkNewComments, dotclear.notify_me.comments.check);
+  setInterval(dotclear.notify_me.checkNewComments, dotclear.notify_me.comments.check);
 });
--- js/post.js
+++ js/post.js
@@ -1,41 +1,29 @@
 /*global dotclear, notifyBrowser */
 'use strict';
 
-function checkCurrentPost() {
-  dotclear.services(
-    'notifyMeCheckCurrentPost',
-    (data) => {
-      try {
-        const response = JSON.parse(data);
-        if (response?.success) {
-          if (response?.payload.ret === 'dirty') {
-            notifyBrowser(response.payload.msg, dotclear.notify_me.config.title, dotclear.notify_me.config.wait);
-            dotclear.notify_me.post.dt = response?.payload.post_dt;
-          }
-        } else {
-          console.log(dotclear.debug && response?.message ? response.message : 'Dotclear REST server error');
-          return;
-        }
-      } catch (e) {
-        console.log(e);
-      }
-    },
-    (error) => {
-      console.log(error);
-    },
-    true, // Use GET method
-    {
-      json: 1, // Use JSON format for payload
-      post_id: dotclear.notify_me.post.id,
-      post_hash: dotclear.notify_me.post.hash,
-      post_dt: dotclear.notify_me.post.dt,
-      post_type: '', // check any type of post
-    },
-  );
-}
-
 window.addEventListener('load', () => {
+  // Load settings
   dotclear.notify_me.post = dotclear.getData('notify_me_post');
+
+  // Helper
+  dotclear.notify_me.checkCurrentPost = () => {
+    dotclear.jsonServicesGet(
+      'notifyMeCheckCurrentPost',
+      (payload) => {
+        if (payload.ret === 'dirty') {
+          notifyBrowser(payload.msg, dotclear.notify_me.config.title, dotclear.notify_me.config.wait);
+          dotclear.notify_me.post.dt = payload.post_dt;
+        }
+      },
+      {
+        post_id: dotclear.notify_me.post.id,
+        post_hash: dotclear.notify_me.post.hash,
+        post_dt: dotclear.notify_me.post.dt,
+        post_type: '', // check any type of post
+      },
+    );
+  };
+
   // Set interval between two checks for current post
-  setInterval(checkCurrentPost, dotclear.notify_me.post.check);
+  setInterval(dotclear.notify_me.checkCurrentPost, dotclear.notify_me.post.check);
 });
