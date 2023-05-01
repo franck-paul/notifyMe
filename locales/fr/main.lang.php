b57770c1d391efad7e9f9d839c6ea9ac5cacfecc
franck <carnet.franck.paul@gmail.com>
2023-05-01 21:51:48 +0200

    l10n

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -22,13 +22,14 @@ L10n::$locales['Wait for user interaction before closing notification'] = 'Atten
 L10n::$locales['Replace Dotclear notifications'] = 'Remplace les notifications Dotclear';
 L10n::$locales['Including Dotclear errors'] = 'Y compris les erreurs Dotclear';
 L10n::$locales['Notifications:'] = 'Notifications :';
-L10n::$locales['Check new comments'] = '';
+L10n::$locales['Check new comments'] = 'Vérifier les nouveaux commentaires';
 L10n::$locales['Only new non-junk comments for the current blog will be checked, whatever is the moderation setting. Your own comments or trackbacks will be ignored.'] = 'Seuls les nouveaux commentaires non indésirables du blog courant seront contrôlés, qu\'ils soient modérés a priori ou a posteriori. Vos propres commentaires et rétroliens seront ignorés.';
 L10n::$locales['Check new comments every (in seconds, default: 30):'] = 'Contrôler les nouveaux commentaires toutes les (en secondes, 30 par défaut) :';
-L10n::$locales['Check current edited post'] = 'Contrôler la publication en cours d\'édition toutes les (en secondes, 60 par défaut) :';
+L10n::$locales['Check current edited post'] = 'Contrôler la publication en cours d\'édition';
 L10n::$locales['Check current edited post every (in seconds, default: 60):'] = 'Contrôler la publication en cours d\'édition toutes les (en secondes, 60 par défaut) :';
 L10n::$locales['One new comment has been posted'] = [
 	'Un nouveau commentaire à été posté',
+	'%s nouveaux commentaires ont été postés',
 	'%s nouveaux commentaires ont été postés',
 ];
 L10n::$locales['Warning: The current entry has been changed elsewhere!'] = 'Attention: La publication en cours d\'édition a été modifiée par ailleurs !';
