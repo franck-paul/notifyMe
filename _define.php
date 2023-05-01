d60724518f5da40b0382f41bfd443ad04f3d8799
franck <carnet.franck.paul@gmail.com>
2023-05-01 21:53:00 +0200

    Release 2.0

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Browser notifications',
     'Display notifications in your web browser',
     'Franck Paul and contributors',
-    '1.2',
+    '2.0',
     [
         'requires'    => [['core', '2.26']],
         'permissions' => dcCore::app()->auth->makePermissions([
