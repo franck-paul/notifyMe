6d13ac3226ed8947339d6ee35db4032270cda95e
franck <carnet.franck.paul@gmail.com>
2023-05-01 21:25:47 +0200

    l10n

---
+++ locales/en/main.lang.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @package Dotclear
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
+ */
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+use Dotclear\Helper\L10n;
+
+L10n::$locales['Browser notifications'] = '';
+L10n::$locales['Display notifications in your web browser'] = '';
+L10n::$locales[' - error'] = '';
+L10n::$locales['Dotclear : %s'] = '';
+L10n::$locales[' - warning'] = '';
+L10n::$locales['Display browser notification'] = '';
+L10n::$locales['The notifications will have to be explicitly granted for the current session before displaying the first one.'] = '';
+L10n::$locales['Wait for user interaction before closing notification'] = '';
+L10n::$locales['Replace Dotclear notifications'] = '';
+L10n::$locales['Including Dotclear errors'] = '';
+L10n::$locales['Notifications:'] = '';
+L10n::$locales['Check new comments'] = '';
+L10n::$locales['Only new non-junk comments for the current blog will be checked, whatever is the moderation setting. Your own comments or trackbacks will be ignored.'] = '';
+L10n::$locales['Check new comments every (in seconds, default: 30):'] = '';
+L10n::$locales['Check current edited post'] = '';
+L10n::$locales['Check current edited post every (in seconds, default: 60):'] = '';
+L10n::$locales['One new comment has been posted'] = [
+	'',
+	'',
+];
+L10n::$locales['Warning: The current entry has been changed elsewhere!'] = '';
