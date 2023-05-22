ea812ba01dc8be84df69300ab7aeb80ae3fb61b0
franck <carnet.franck.paul@gmail.com>
2023-05-22 16:11:10 +0200

    Minify fix

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Browser notifications',
     'Display notifications in your web browser',
     'Franck Paul and contributors',
-    '2.2.2',
+    '2.2.3',
     [
         'requires'    => [['core', '2.26']],
         'permissions' => dcCore::app()->auth->makePermissions([
