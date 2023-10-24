29de3bf7ab5f37b562dcdeeb19044605f07311df
franck <carnet.franck.paul@gmail.com>
2023-10-24 15:37:59 +0200

    New release 4.0

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Browser notifications',
     'Display notifications in your web browser',
     'Franck Paul and contributors',
-    '3.0',
+    '4.0',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
