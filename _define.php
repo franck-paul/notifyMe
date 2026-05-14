94baa2180a2bb679c5c71ba827d7da1d20bafe93
franck <contact@open-time.net>
2026-05-14 15:53:21 +0200

    New release 7.0

--- _define.php
+++ _define.php
@@ -15,9 +15,9 @@ $this->registerModule(
     'Browser notifications',
     'Display notifications in your web browser',
     'Franck Paul and contributors',
-    '6.2',
+    '7.0',
     [
-        'date'        => '2026-03-31T17:29:11+0200',
+        'date'        => '2026-05-14T15:53:21+0200',
         'requires'    => [['core', '2.38']],
         'permissions' => 'My',
         'type'        => 'plugin',
